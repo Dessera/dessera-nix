c0310dbb6dc85f02ad8282a6515364e74ad7d0e8
Dessera <1533653159@qq.com>
2024-07-27 19:33:37 +0800

    feat: Add fcitx5 input method in global

--- entries/dessera-nix/locale.nix
+++ entries/dessera-nix/locale.nix
@@ -1,9 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   time.timeZone = "Asia/Shanghai";
   time.hardwareClockInLocalTime = true;
   i18n = {
     defaultLocale = "zh_CN.UTF-8";
+
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5 = {
+        addons = [
+          pkgs.fcitx5-rime
+          pkgs.fcitx5-mozc
+          pkgs.fcitx5-gtk
+          pkgs.fcitx5-qt
+          pkgs.fcitx5-configtool
+          pkgs.fcitx5-chinese-addons
+        ];
+        waylandFrontend = true;
+      };
+    };
   };
 }
