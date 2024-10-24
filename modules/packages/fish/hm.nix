c4e705f75ed1f3478e6b41d5769e55a71ca92aed
Dessera <lord.changed@foxmail.com>
2024-10-24 22:49:55 +0800

    更新了我的git邮箱

--- modules/packages/fish/hm.nix
+++ modules/packages/fish/hm.nix
@@ -18,25 +18,16 @@ in
     programs.fish = {
       enable = true;
       plugins = [
-        {
-          name = "pure";
-          src = pkgs.fetchFromGitHub {
-            owner = "pure-fish";
-            repo = "pure";
-            rev = "v4.11.0";
-            sha256 = "1vlxa9va9j616ajbcw9amqgz43cib2vds6yd56dp3rad9wynlg7k";
-          };
-        }
-        {
-          name = "gitnow";
-          src = pkgs.fetchFromGitHub {
-            owner = "joseluisq";
-            repo = "gitnow";
-            rev = "2.12.0";
-            sha256 = "0559c5j2gsxdg6g2bmk7qws3x0842g2y55ymd2xf20crcv12psiy";
-          };
-        }
+        { inherit (pkgs.fishPlugins.pure) name src; }
+        { inherit (pkgs.fishPlugins.grc) name src; }
+        { inherit (pkgs.fishPlugins.pisces) name src; }
+        { inherit (pkgs.fishPlugins.plugin-git) name src; }
+        { inherit (pkgs.fishPlugins.sponge) name src; }
       ];
     };
+
+    home.packages = with pkgs; [
+      grc
+    ];
   };
 }
--- modules/packages/git/hm.nix
+++ modules/packages/git/hm.nix
@@ -13,7 +13,7 @@ in
     programs.git = {
       enable = true;
       userName = "Dessera";
-      userEmail = "1533653159@qq.com";
+      userEmail = "lord.changed@foxmail.com";
     };
   };
 }
