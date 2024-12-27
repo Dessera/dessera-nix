3ec64a9aa82966c77797282cbb7d3481e6991f4c
Dessera <dessera@qq.com>
2024-12-27 14:09:20 +0800

    暂时移除gtk主题配置

--- modules/hm/desktop/gtk.nix
+++ modules/hm/desktop/gtk.nix
@@ -24,7 +24,7 @@ in
     };
 
     # ! catppuccin no longer supports the gtk, but it is still possible to use the theme
-    catppuccin.gtk.enable = true;
+    # catppuccin.gtk.enable = true;
 
     # force unset the gtk2 configuration
     # for unknown reason, the gtk2 configuration will cause the home-manager to fail
