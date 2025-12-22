85302e40faf9641dbf6bc87c24f1a0970d6c4782
Dessera <dessera@qq.com>
2025-12-22 20:26:34 +0800

    use rime instead of chinese-addons

--- components/nixos/locale.nix
+++ components/nixos/locale.nix
@@ -13,12 +13,20 @@
       type = "fcitx5";
       fcitx5 = {
         addons = with pkgs; [
-          fcitx5-rime
-          fcitx5-mozc
+          (fcitx5-rime.override {
+            rimeDataPkgs = [
+              rime-data
+              rime-ice
+            ];
+          })
+
           fcitx5-gtk
+          fcitx5-lua
           qt6Packages.fcitx5-qt
           qt6Packages.fcitx5-configtool
           qt6Packages.fcitx5-chinese-addons
+
+          fcitx5-tokyonight
         ];
 
         waylandFrontend = true;
